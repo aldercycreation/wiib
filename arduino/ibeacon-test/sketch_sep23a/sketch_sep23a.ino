4c885e7dde737226d04bf0589327b40b981e63cd
Muzaa <muzhafarahzan@Muza-MacBook.local>
2016-09-23 02:58:15 +0800

    arduino can now broadcast custom SSID.

--- arduino/ibeacon-test/sketch_sep23a/sketch_sep23a.ino
+++ arduino/ibeacon-test/sketch_sep23a/sketch_sep23a.ino
@@ -1,100 +1,121 @@
 #include <ESP8266WiFi.h>
- 
-const char* ssid = "Magesh";
-const char* password = "jayakumar";
- 
-int ledPin = 13; // GPIO13
+
+//////////////////////
+// WiFi Definitions //
+//////////////////////
+const char WiFiAPPSK[] = "sparkfun";
+
+/////////////////////
+// Pin Definitions //
+/////////////////////
+const int LED_PIN = 5; // Thing's onboard, green LED
+const int ANALOG_PIN = A0; // The only analog pin on the Thing
+const int DIGITAL_PIN = 12; // Digital pin to be read
+
 WiFiServer server(80);
- 
-void setup() {
-  Serial.begin(115200);
-  delay(10);
- 
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
- 
-  // Connect to WiFi network
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
- 
-  WiFi.begin(ssid, password);
- 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
- 
-  // Start the server
+
+void setup() 
+{
+  initHardware();
+  setupWiFi();
   server.begin();
-  Serial.println("Server started");
- 
-  // Print the IP address
-  Serial.print("Use this URL to connect: ");
-  Serial.print("http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("/");
- 
+  Serial.begin(9600);
 }
- 
-void loop() {
+
+void loop() 
+{
   // Check if a client has connected
   WiFiClient client = server.available();
   if (!client) {
     return;
   }
- 
-  // Wait until the client sends some data
-  Serial.println("new client");
-  while(!client.available()){
-    delay(1);
-  }
- 
+
   // Read the first line of the request
-  String request = client.readStringUntil('\r');
-  Serial.println(request);
+  String req = client.readStringUntil('\r');
+  Serial.println(req);
   client.flush();
- 
+  
   // Match the request
- 
-  int value = LOW;
-  if (request.indexOf("/LED=ON") != -1)  {
-    digitalWrite(ledPin, HIGH);
-    value = HIGH;
+  int val = -1; // We'll use 'val' to keep track of both the
+                // request type (read/set) and value if set.
+  if (req.indexOf("/led/0") != -1)
+    val = 0; // Will write LED low
+  else if (req.indexOf("/led/1") != -1)
+    val = 1; // Will write LED high
+  else if (req.indexOf("/read") != -1)
+    val = -2; // Will print pin reads
+  // Otherwise request will be invalid. We'll say as much in HTML
+
+  // Set GPIO5 according to the request
+  if (val >= 0)
+    digitalWrite(LED_PIN, val);
+  
+  client.flush();
+
+  // Prepare the response. Start with the common header:
+  String s = "HTTP/1.1 200 OK\r\n";
+  s += "Content-Type: text/html\r\n\r\n";
+  s += "<!DOCTYPE HTML>\r\n<html>\r\n";
+  // If we're setting the LED, print out a message saying we did
+  if (val >= 0)
+  {
+    s += "LED is now ";
+    s += (val)?"high":"low";
   }
-  if (request.indexOf("/LED=OFF") != -1)  {
-    digitalWrite(ledPin, LOW);
-    value = LOW;
+  else if (val == -2)
+  { // If we're reading pins, print out those values:
+    s += "Analog Pin = ";
+    s += String(analogRead(ANALOG_PIN));
+    s += "<br>"; // Go to the next line.
+    s += "Digital Pin 12 = ";
+    s += String(digitalRead(DIGITAL_PIN));
   }
- 
-// Set ledPin according to the request
-//digitalWrite(ledPin, value);
- 
-  // Return the response
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: text/html");
-  client.println(""); //  do not forget this one
-  client.println("<!DOCTYPE HTML>");
-  client.println("<html>");
- 
-  client.print("Led pin is now: ");
- 
-  if(value == HIGH) {
-    client.print("On");
-  } else {
-    client.print("Off");
+  else
+  {
+    s += "Invalid Request.<br> Try /led/1, /led/0, or /read.";
   }
-  client.println("<br><br>");
-  client.println("<a href=\"/LED=ON\"\"><button>Turn On </button></a>");
-  client.println("<a href=\"/LED=OFF\"\"><button>Turn Off </button></a><br />");  
-  client.println("</html>");
- 
+  s += "</html>\n";
+
+  // Send the response to the client
+  client.print(s);
   delay(1);
   Serial.println("Client disonnected");
-  Serial.println("");
- 
+
+  // The client will actually be disconnected 
+  // when the function returns and 'client' object is detroyed
 }
- 
+
+void setupWiFi()
+{
+  WiFi.mode(WIFI_AP);
+  
+  // Do a little work to get a unique-ish name. Append the
+  // last two bytes of the MAC (HEX'd) to "Thing-":
+  uint8_t mac[WL_MAC_ADDR_LENGTH];
+  WiFi.softAPmacAddress(mac);
+  String macID = String(mac[WL_MAC_ADDR_LENGTH - 2], HEX) +
+                 String(mac[WL_MAC_ADDR_LENGTH - 1], HEX);
+  macID.toUpperCase();
+
+
+  
+  String AP_NameString = macID+"|DATA|";
+  
+  char AP_NameChar[AP_NameString.length() + 1];
+  memset(AP_NameChar, AP_NameString.length() + 1, 0);
+  
+  for (int i=0; i<AP_NameString.length(); i++)
+    AP_NameChar[i] = AP_NameString.charAt(i);
+  
+  WiFi.softAP(AP_NameChar, WiFiAPPSK);
+}
+
+void initHardware()
+{
+  Serial.begin(115200);
+  pinMode(DIGITAL_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+  // Don't need to set ANALOG_PIN as input, 
+  // that's all it can be.
+}
